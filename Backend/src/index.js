7fab17b8e1173adb744c75227d51dee8563d284e
Shantanu675 <shantanudanej05@gmail.com>
2025-08-28 02:40:09 +0530

    .

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://quiz-1-dnu2.onrender.com', 'https://quiz-application-shantanu.netlify.app'],
+  // origin: ['http://localhost:5173', 'https://quiz-1-dnu2.onrender.com', 'https://quiz-application-shantanu.netlify.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
